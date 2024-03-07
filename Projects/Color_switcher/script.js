ab5cd79851a29753ce96f57241ed3423abd3c66a
KinjalShah10 <shahkinjal0813@gmail.com>
2024-03-07 11:36:08 +0530

    filter

---
+++ Projects/Color_switcher/script.js
@@ -0,0 +1,26 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+//for each is used for node list 
+buttons.forEach(function(button)
+{
+    console.log(button);
+    button.addEventListener('click', function(e)
+    {
+        console.log(e);
+        console.log(e.target);
+        if(e.target.id === "red"){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === "black"){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === "yellow"){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === "orange"){
+            body.style.backgroundColor = e.target.id;
+        }
+
+    });
+});
